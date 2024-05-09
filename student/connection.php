da4af3f395fc283cfce681f662212e48fcd4d5e4
Haytham Failali Jatabi <93624059+Failalihaytam@users.noreply.github.com>
2024-05-09 15:24:13 +0000

    Thu, May 9, 2024, 4:24 PM +01:00

--- student/connection.php
+++ student/connection.php
@@ -1,11 +1,14 @@
 <?php
-$server_name = "localhost";
-$username = "root";
-$password ="";
-$database = "project";
+$server_name = "localhost"; // Nom du serveur MySQL
+$username = "root"; // Nom d'utilisateur MySQL
+$password =""; // Mot de passe MySQL
+$database = "project"; // Nom de la base de données
 
-$conn = mysqli_connect($server_name,$username,$password,$database);
+$conn = mysqli_connect($server_name, $username, $password, $database); // Connexion à la base de données MySQL
 
 if (!$conn) {
-    die("Connection failed: ". mysqli_connect_error());
+    // Vérification de la connexion réussie à la base de données
+    // Si la connexion échoue, affiche un message d'erreur avec les détails de l'erreur
+    die("La connexion a échoué: " . mysqli_connect_error());
 }
+?>
